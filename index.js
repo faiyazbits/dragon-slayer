5c4b4e92c1f6b984eeee4da44fecebdd1893d698
jroshansafi <85227612+jroshansafi@users.noreply.github.com>
2022-04-13 11:39:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const log = console.log;
 const name = prompt("What is your name? ");
 log(chalk.red(name));
 
-const officeRooms = {
+const rooms = {
     'Great Hall': {'East': 'Crossbow','West':undefined, 'North': 'BedRoom', 'South': 'Cellar', 'items': 'ChestPlate' },
     'BedRoom': {'East': undefined,'West':undefined, 'North': undefined, 'South': 'Great Hall', 'items': 'Armory'},
     'Cellar': {'East': undefined,'West':undefined, 'North': 'Great Hall', 'South': undefined, 'items': 'Key'},
