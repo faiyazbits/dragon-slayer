debf818d9ede848705ae993919e613af5bdadefb
Hidhaya <hidhaya@provility.com>
2022-04-13 17:48:25 +0530

    added game instructions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const prompt = require('prompt-sync')();
 const chalk = require('chalk');
+const { yellow } = require('colors');
 
 
 const log = console.log;
@@ -20,12 +21,27 @@ const rooms = {
 }
     
     
-    log(rooms.GreatHall.items);
-    log(chalk.blue(rooms['Exit'].North));
-   
+// log(rooms.GreatHall.items);
+    // log(chalk.blue(rooms['Exit'].North));
+    log(chalk.green(rooms.GreatHall.items));
     
+const gameIntroduction = "You are in a small village under attack from a Dragon!Quick, enter the building and get your crossbow."
 
-    
+log(chalk.red(gameIntroduction));
+
+const gameCommands = `You can use the following commands:
+WALK- move north, south, east, west(ex:walk south)
+MOVE- move an entity (ex: move sword)
+COLLECT- collect an entity (ex: collect key)`
+
+log(chalk.yellow(gameCommands));
 
 
+const inventories = `You can use inventory to view your items(ex: inventory)['sword']
+You have the following items: ['sword']`
+log(chalk.white(inventories));
 
+const greatHallIntro = `You are located at: GreatHall -=- The great hall at the villages center. There is a weapon cache at the back
+Objects:
+armory`
+log(chalk.grey(greatHallIntro));
