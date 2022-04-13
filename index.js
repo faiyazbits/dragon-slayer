387b95cd293cc18c0baae0a9adfd70976c139eeb
reshmaresh02 <reshmukh2003@gmail.com>
2022-04-13 15:37:36 +0530

    remodified code and added starting room inventory details

--- index.js
+++ index.js
@@ -22,16 +22,21 @@ log(rooms['Cellar'].West)
 log(chalk.rgb(105, 41, 224)(rooms['BedRoom'].items))
 
 const storyIntro = "You are in a small Village under attack from a Dragon! Quick, enter the building and get your Crossbow"
-setTimeout(() => {log(chalk.red(storyIntro)); }, 3000); 
+log(chalk.red(storyIntro))
 
 
 const storyCommands = `You can use the following commands:
 WALK- move north, south, east, west(ex:walk south)
 MOVE- move an entity (ex: move sword)
 COLLECT- collect an entity (ex: collect key)`
-setTimeout(() => {log(chalk.rgb(238,238,174)(storyCommands));}, 5000);
+ log(chalk.rgb(238,238,174)(storyCommands))
 
 const inventories = `You can use inventory to view your items(ex: inventory)['Chestplate']
 You have the following items: ['Chestplate']`
-setTimeout(() => {log(chalk.white(inventories)); }, 7000); 
+log(chalk.white(inventories)); 
+
+const greatHallInformation = `You are located at: GreatHall -=- The great hall at the villages center. There is a weapon cache at the back
+Objects:
+armory`
+log(chalk.grey(greatHallInformation))
 
