3f83e64fcec1bb3083e67a3ea4a6ebdac891e269
reshmaresh02 <reshmukh2003@gmail.com>
2022-04-13 11:38:36 +0530

    codes for representing rooms

--- index.js
+++ index.js
@@ -7,4 +7,21 @@ const log = console.log;
 
 
 const name = prompt("What is your name? ");
-log(chalk.red(name));
+log(chalk.blue(name));
+
+const fav = prompt("ur favorite food?");
+ log(chalk.yellow(fav));
+ 
+ const rooms = {
+    'Great hall':{ 'South':'BedRoom', 'East': 'Exit','items':['Crossbow', 'Armory'] },
+    
+    'BedRoom' :{'North':'Great hall', 'East':'Cellar','items':['Chestplate', 'Key'] },
+
+    'Cellar' : {'North':'Exit','West':'BedRoom','items':'Shield' },
+
+    'Exit': {'South':'Cellar','West':'Great hall' },
+    
+
+}
+log(rooms['Cellar'].North)
+log(chalk.yellow(rooms['BedRoom'].items))
