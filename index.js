fde072a59aff5589f169d5f350385c4aed6be86b
Hidhaya <hidhaya@provility.com>
2022-04-13 13:33:45 +0530

    created rooms

--- index.js
+++ index.js
@@ -8,3 +8,24 @@ const log = console.log;
 
 const name = prompt("What is your name? ");
 log(chalk.red(name));
+
+
+
+const rooms = {
+
+    'GreatHall': {'East': 'Cellar','North': 'BedRoom', 'South': 'Exit', 'items': ['Crossbow', 'Shield'] },
+    'BedRoom': {'South': 'GreatHall', 'items': ['Armory', 'Key']},
+    'Cellar': {'West': 'GreatHall', 'items': ['ChestPlate']},
+    'Exit': {'North':'GreatHall', 'items': []},
+}
+    
+    
+    log(rooms.GreatHall.items);
+    log(chalk.blue(rooms['Exit'].North));
+   
+    
+
+    
+
+
+
