1d634f2a0bf837b8dcdaa4578398922e695a53e3
jroshansafi <jroshansafi@gmail.com>
2022-04-13 11:25:39 +0530

    represented rooms using names,items,direction

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ const log = console.log;
 
 const name = prompt("What is your name? ");
 log(chalk.red(name));
+
+const officeRooms = {
+    'Great Hall': {'East': 'Crossbow','West':undefined, 'North': 'BedRoom', 'South': 'Cellar', 'items': 'ChestPlate' },
+    'BedRoom': {'East': undefined,'West':undefined, 'North': undefined, 'South': 'Great Hall', 'items': 'Armory'},
+    'Cellar': {'East': undefined,'West':undefined, 'North': 'Great Hall', 'South': undefined, 'items': 'Key'},
+    'Exit': {'East': undefined,'West':'Great Hall', 'North': undefined, 'South': undefined, 'items': ['Crossbow', 'Shield']},
+}
+ log(officeRooms['Great Hall'])
+ log(officeRooms['Exit'].items)
+
+
+
