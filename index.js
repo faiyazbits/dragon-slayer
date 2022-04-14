3cec103beebcb3af03572775a45801e113f15bb0
Hidhaya <hidhaya@provility.com>
2022-04-14 15:57:12 +0530

    modified code, added player location

--- index.js
+++ index.js
@@ -1,47 +1,61 @@
-
-const prompt = require('prompt-sync')();
-const chalk = require('chalk');
-const { yellow } = require('colors');
-
+const prompt = require("prompt-sync")();
+const chalk = require("chalk");
+const { yellow } = require("colors");
 
 const log = console.log;
-
 
 const name = prompt("What is your name? ");
 log(chalk.red(name));
 
-
-
 const rooms = {
+  GreatHall: {
+    East: "Cellar",
+    North: "BedRoom",
+    South: "Exit",
+    items: ["Crossbow", "Shield"],
+  },
+  BedRoom: { South: "GreatHall", items: ["Armory", "Key"] },
+  Cellar: { West: "GreatHall", items: ["ChestPlate"] },
+  Exit: { North: "GreatHall", items: [] },
+};
 
-    'GreatHall': {'East': 'Cellar','North': 'BedRoom', 'South': 'Exit', 'items': ['Crossbow', 'Shield'] },
-    'BedRoom': {'South': 'GreatHall', 'items': ['Armory', 'Key']},
-    'Cellar': {'West': 'GreatHall', 'items': ['ChestPlate']},
-    'Exit': {'North':'GreatHall', 'items': []},
-}
-    
-    
-// log(rooms.GreatHall.items);
-    // log(chalk.blue(rooms['Exit'].North));
-    log(chalk.green(rooms.GreatHall.items));
-    
-const gameIntroduction = "You are in a small village under attack from a Dragon!Quick, enter the building and get your crossbow."
+// Dictionary of main rooms and their corresponding descriptions
+main_rooms = {
+  "GreatHall":
+    "The great hall at the villages center. There is a weapon cache at the back",
+  Bedroom: "The chiefs room, although he is no where to be found",
+  Cellar:
+    "It smells foul and damp in here. The cries of prisoners can be heard echoing on the cracked stone walls.",
+  Exit: "Goodbye! You can type leave to end the game.",
+};
 
-log(chalk.red(gameIntroduction));
+// setTimeout(function () {
+//   const gameIntroduction =
+//     "You are in a small village under attack from a Dragon!Quick, enter the building and get your crossbow.";
+//   log(chalk.red(gameIntroduction));
+// }, 500);
 
 const gameCommands = `You can use the following commands:
 WALK- move north, south, east, west(ex:walk south)
 MOVE- move an entity (ex: move sword)
-COLLECT- collect an entity (ex: collect key)`
+COLLECT- collect an entity (ex: collect key)`;
 
 log(chalk.yellow(gameCommands));
 
-
 const inventories = `You can use inventory to view your items(ex: inventory)['sword']
-You have the following items: ['sword']`
+You have the following items: ['sword']`;
 log(chalk.white(inventories));
 
-const greatHallIntro = `You are located at: GreatHall -=- The great hall at the villages center. There is a weapon cache at the back
+const greatHallIntro = `You are located at: GreatHall -=- ${main_rooms.GreatHall}
 Objects:
-armory`
+${rooms.GreatHall.items}`//Crossbow, Shield
 log(chalk.grey(greatHallIntro));
+
+const playerCurrentLocation = rooms.GreatHall.East;
+log(chalk.red(playerCurrentLocation));//Cellar
+
+if(playerCurrentLocation == rooms.GreatHall.East){
+  log(chalk.red(`You are located at:Cellar -=- ${main_rooms.Cellar} items: ${rooms.Cellar.items}`));
+}
+
+
