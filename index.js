80604292424b01242492d1bb867254c26ed72980
reshmaresh02 <reshmukh2003@gmail.com>
2022-04-14 22:57:44 +0530

    changed two objects into one and variable name direction

--- index.js
+++ index.js
@@ -10,27 +10,18 @@ const name = prompt("What is your name? ");
 log(chalk.blue(name));
 
 const rooms = {
-    'GreatHall':{'Direction':{'North':'BedRoom','South':'Exit', 'East':'Cellar'}, 'items':['Crossbow','Shield'] },
-    
-    'BedRoom' :{'South':'GreatHall','items':['Armory', 'Key'] },
-
-    'Cellar' : {'West':'GreatHall','items':['Chestplate'] },
-
-    'Exit': {'North':'GreatHall','items':[]},
+    'GreatHall': {'Direction':{'East': 'Cellar','North': 'BedRoom', 'South': 'Exit'},
+    'Description': 'The great hall at the villages center. There is a weapon cache at the back', 'items': ['Crossbow', 'Shield'] },
+    'BedRoom': {'Direction':{'South': 'GreatHall'}, 'Description': "The chiefs room, although he is no where to be found", 'items': ['Armory', 'Key']},
+    'Cellar': {'Direction':{'West': 'GreatHall'}, 'Description': "It smells foul and damp in here. The cries of prisoners can be heard echoing on the cracked stone walls.", 'items': ['ChestPlate']},
+    'Exit': {'Direction':{'North':'GreatHall'},'Description': "Goodbye! You can type leave to end the game.", 'items': []},
 }
+
 log(rooms['Cellar'].West)
 log(chalk.rgb(105, 41, 224)(rooms['BedRoom'].items))
 
-main_rooms = {"GreatHall":"The great hall at the villages center. There is a weapon cache at the back",
-       "Bedroom":"The chiefs room, although he is no where to be found",
-       "Cellar":"It smells foul and damp in here. The cries of prisoners can be heard echoing on the cracked stone walls.",
-       "Exit":"Goodbye! You can type leave to end the game."}
-
-log(main_rooms['GreatHall'])
-
 const storyIntro = "You are in a small Village under attack from a Dragon! Quick, enter the building and get your Crossbow"
 log(chalk.red(storyIntro))
-
 
 const storyCommands = `You can use the following commands:
 WALK- move north, south, east, west(ex:walk south)
@@ -42,22 +33,22 @@ const inventories = `You can use inventory to view your items(ex: inventory)['Ch
 You have the following items: ['Chestplate']`
 log(chalk.white(inventories)); 
 
-const greatHallInformation = `You are located at: GreatHall -=- ${main_rooms['GreatHall']}
+const greatHallInformation = `You are located at: GreatHall -=- ${rooms['GreatHall'].Description}
 Objects:
 ${rooms['GreatHall'].items}`
 log(chalk.grey(greatHallInformation))
 
-const direction = prompt("whats your next move?");
-log(chalk.blue(direction));
+const nextMove = prompt("whats your next move?");
+log(chalk.blue(nextMove));
 
-function move(){
-    if (direction == (rooms.GreatHall.Direction))
+function directionMove(){
+    if (nextMove == (rooms['GreatHall'].Direction))
 console.log("Enter The Room")
     else {
-        console.log("Try again, your currently available exits are" , (rooms.GreatHall.Direction))
+        console.log("Try again, your currently available exits are" , (rooms['GreatHall'].Direction))
     }
 }
-move()
+directionMove()
 
 
 
