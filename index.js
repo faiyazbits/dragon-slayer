f29c33daa53e7b4a2f81d1bfcd25d0cf603d5faf
reshmaresh02 <reshmukh2003@gmail.com>
2022-04-14 18:08:42 +0530

    modified code without hardcoring and trying if statement for next move

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const name = prompt("What is your name? ");
 log(chalk.blue(name));
 
 const rooms = {
-    'GreatHall':{'North':'BedRoom','South':'Exit', 'East':'Cellar', 'items':['Crossbow','Shield'] },
+    'GreatHall':{'Direction':{'North':'BedRoom','South':'Exit', 'East':'Cellar'}, 'items':['Crossbow','Shield'] },
     
     'BedRoom' :{'South':'GreatHall','items':['Armory', 'Key'] },
 
@@ -21,6 +21,13 @@ const rooms = {
 log(rooms['Cellar'].West)
 log(chalk.rgb(105, 41, 224)(rooms['BedRoom'].items))
 
+main_rooms = {"GreatHall":"The great hall at the villages center. There is a weapon cache at the back",
+       "Bedroom":"The chiefs room, although he is no where to be found",
+       "Cellar":"It smells foul and damp in here. The cries of prisoners can be heard echoing on the cracked stone walls.",
+       "Exit":"Goodbye! You can type leave to end the game."}
+
+log(main_rooms['GreatHall'])
+
 const storyIntro = "You are in a small Village under attack from a Dragon! Quick, enter the building and get your Crossbow"
 log(chalk.red(storyIntro))
 
@@ -29,14 +36,36 @@ const storyCommands = `You can use the following commands:
 WALK- move north, south, east, west(ex:walk south)
 MOVE- move an entity (ex: move sword)
 COLLECT- collect an entity (ex: collect key)`
- log(chalk.rgb(238,238,174)(storyCommands))
+log(chalk.rgb(238,238,174)(storyCommands))
 
 const inventories = `You can use inventory to view your items(ex: inventory)['Chestplate']
 You have the following items: ['Chestplate']`
 log(chalk.white(inventories)); 
 
-const greatHallInformation = `You are located at: GreatHall -=- The great hall at the villages center. There is a weapon cache at the back
+const greatHallInformation = `You are located at: GreatHall -=- ${main_rooms['GreatHall']}
 Objects:
-armory`
+${rooms['GreatHall'].items}`
 log(chalk.grey(greatHallInformation))
 
+const direction = prompt("whats your next move?");
+log(chalk.blue(direction));
+
+function move(){
+    if (direction == (rooms.GreatHall.Direction))
+console.log("Enter The Room")
+    else {
+        console.log("Try again, your currently available exits are" , (rooms.GreatHall.Direction))
+    }
+}
+move()
+
+
+
+
+
+// function  delayedLog(time){
+//     let delay = setInterval(() => {console.log(time)}, 8000);
+//     clearInterval(() =>{console.log(delay)},3000)
+
+// }
+// delayedLog()
