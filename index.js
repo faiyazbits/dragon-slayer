f0d3f677635e52cabaff2089753c71cb85e8f14e
jroshansafi <jroshansafi@gmail.com>
2022-04-14 13:12:15 +0530

    datas been used without hardcode

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const prompt = require('prompt-sync')();
 const chalk = require('chalk');
+const { textChangeRangeIsUnchanged } = require('typescript');
 
 
 const log = console.log;
@@ -15,8 +16,15 @@ const rooms = {
     'Cellar': {'West': 'GreatHall', 'items': ['ChestPlate']},
     'Exit': {'North':'GreatHall', 'items': []},
 }
+//Dictionary of main rooms and their corresponding descriptions
+const main_rooms= {
+  "GreatHall":"The great hall at the villages center. There is a weapon cache at the back",
+  "Bedroom":"The chiefs room, although he is no where to be found",
+  "Cellar":"It smells foul and damp in here. The cries of prisoners can be heard echoing on the cracked stone walls.",
+  "Exit":"Goodbye! You can type leave to end the game."
+}
 
- const storyLine = "You are in a small village under attack from a dragon! Quick, enter the building and get your crossbow"
+const storyLine = "You are in a small village under attack from a dragon! Quick, enter the building and get your crossbow"
 log(chalk.red(storyLine))
 
 const storyCommands = `You can use the following commands:
@@ -30,10 +38,33 @@ const inventories = `You can use inventory to view your items(ex: inventory)['sh
 You have the following items: ['shield']`
 log(chalk.rgb(245,245,245)(inventories))
 
-const greatHallDescription = `You are located at: GreatHall -=- The great hall at the villages center. There is a weapon cache at the back
+const greatHallDescription = `You are located at: GreatHall -=- ${main_rooms['GreatHall']} 
 Objects:
-armory`
+${rooms['GreatHall'].items}`//Crossbow, Shield
 log(chalk.rgb(145,141,141)(greatHallDescription))
+
+// to display a text using setInterval method
+function delayedLog() {
+strings = [];
+for (var i = 1; i <= storyLine.length; i++)
+  strings.push (storyLine.substring (0, i));
+ var position = 0;
+setInterval (() => {
+  console.log (strings [position++]);
+  if (position == strings.length) position = 0;
+}, 500);
+}
+//delayedLog()
+// function myStopFunction() {
+//     clearInterval(out);
+//   }
+//  myStopFunction()
+
+ 
+    
+  
+ 
+
 
 
 
