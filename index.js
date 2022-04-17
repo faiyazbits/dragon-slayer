8b85114da9528bb08b3a774d7ca3bd3bdd3683cf
Fawazahmed12 <fawaz@provility.com>
2022-04-17 14:22:39 +0530

    modified rooms object and added if else condition

--- index.js
+++ index.js
@@ -6,25 +6,19 @@ const chalk = require('chalk');
 const log = console.log;
 
 
-const name = prompt("What is your name? ");
-log(chalk.red(name));
-
-
+// const name = prompt("What is your name? ");
+// log(chalk.red(name));
 
 
 const rooms = {
-    'GreatHall': {'East': 'Cellar','North': 'BedRoom', 'South': 'Exit', 'items': ['Crossbow', 'Shield'] },
-    'BedRoom': {'South': 'GreatHall', 'items': ['Armory', 'Key']},
-    'Cellar': {'West': 'GreatHall', 'items': ['ChestPlate']},
-    'Exit': {'North':'GreatHall', 'items': []},
-    'main_rooms': {
-        "Great Hall":"The great hall at the villages center. There is a weapon cache at the back",
-        "Bedroom":"The chiefs room, although he is no where to be found",
-        "Cellar":"It smells foul and damp in here. The cries of prisoners can be heard echoing on the cracked stone walls.",
-        "Exit":"Goodbye! You can type leave to end the game."
-        }
-    }
-
+    'greatHall': {
+      'directions': { 'east': ['cellar'], 'north': ['bedRoom'], 'south': ['exit']},
+      'description': 'The great hall at the villages center. There is a weapon cache at the back', 'items': ['crossbow', 'shield']
+    },
+    'bedRoom': { 'directions': { 'south': ['greatHall'] }, 'description': "The chiefs room, although he is no where to be found", 'items': ['armory', 'key'] },
+    'cellar': { 'directions': { 'west': ['greatHall'] }, 'description': "It smells foul and damp in here. The cries of prisoners can be heard echoing on the cracked stone walls.", 'items': ['chestplate'] },
+    'exit': { 'directions': { 'north': ['greatHall'] }, 'description': "Goodbye! You can type leave to end the game.", 'items': [] },
+  }
 
 
 const storyLine = "You are in a small village under attack from a dragon! Quick, enter the building and get your crossbow"
@@ -41,8 +35,29 @@ const inventories = `You can use inventory to view your items(ex: inventory)['sh
 You have the following items: ['shield']`
 log(chalk.white(inventories))
 
-const greatHallDescription = `You are located at: GreatHall -=- ${rooms.main_rooms['Great Hall']} 
+const greatHallDescription = `You are located at: GreatHall -=- ${rooms['greatHall'].description} 
 Objects:
-${rooms['GreatHall'].items}`
+${rooms['greatHall'].items}`
 log(chalk.grey(greatHallDescription))
+
+
+
+const nextMove = prompt("What's your next move? ");//walk west
+log(chalk.white("-=-", (nextMove)));
+
+function directionMove() {
+  if (nextMove == "walk east") {
+    console.log("Enter The Cellar")
+  }
+  else if (nextMove == "walk north") {
+    console.log("enter the bedroom")
+  }
+  else if (nextMove == "walk south") {
+    console.log("exit")
+  }
+  else {
+    console.log("Try again, your currently available exits are", (rooms['greatHall'].directions))
+  }
+}
+directionMove()
 
