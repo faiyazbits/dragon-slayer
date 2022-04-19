01542aa4038bc0c96f7bfd3554a012d78f3f1404
jroshansafi <85227612+jroshansafi@users.noreply.github.com>
2022-04-19 14:27:39 +0530

    Update description.js

--- description.js
+++ description.js
@@ -1,6 +1,5 @@
 const chalk = require('chalk');
 const log = console.log;
-// const {rooms} = require("./index.js");
 
 exports.generateDescription= function generateDescription(name, description,items){
 console.log(`You are located at: ${name} -=- ${description} 
