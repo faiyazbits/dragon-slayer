3c3749d2b277195dcf39a1a8fcb48046432b66ad
jroshansafi <jroshansafi@gmail.com>
2022-04-19 14:21:07 +0530

    wrong direction, error message used

--- description.js
+++ description.js
@@ -1,22 +1,9 @@
+const chalk = require('chalk');
+const log = console.log;
+// const {rooms} = require("./index.js");
 
-// const greatHallDescription = `You are located at: ${rooms.greatHall.name} -=- ${rooms['greatHall'].description} 
-// Objects:
-// ${rooms['greatHall'].items}`//Crossbow, Shield
-// log(chalk.rgb(145, 141, 141)(greatHallDescription))
-
-// let nextMove = prompt("What's your next move? ");//walk west
-// log(chalk.rgb(145, 141, 141)("-=- walk", (nextMove)));
-
-// const bedRoomDescription = `You are located at: ${rooms.bedRoom.name} -=- ${rooms['bedRoom'].description} 
-// Objects:
-// ${rooms['bedRoom'].items}`
-
-// const cellarDescription = `You are located at: ${rooms.cellar.name} -=- ${rooms['cellar'].description} 
-// Objects:
-// ${rooms['cellar'].items}`
-
-// const exitDescription = `You are located at: ${rooms.exit.name} -=- ${rooms['exit'].description}`
-
-function generateDescription(){
-    console.log(`you are located at ${name}: -=- ${description}`)
+exports.generateDescription= function generateDescription(name, description,items){
+console.log(`You are located at: ${name} -=- ${description} 
+Objects:
+${items}`)
 }
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const {storyIntro} = require('./intoduction.js');
+const { storyIntro } = require('./intoduction.js');
+const { generateDescription } = require('./description.js');
+
 
 const prompt = require('prompt-sync')();
 const chalk = require('chalk');
@@ -8,74 +10,38 @@ const name = prompt("What is your name? ");
 log(chalk.red(name));
 
 const rooms = {
-  'greatHall': { name: 'GreatHall',
-    'directions': { 'east': ['cellar'], 'north': ['bedRoom'], 'south': ['exit']},
+  'greatHall': {
+    name: 'GreatHall',
+    'directions': { 'east': 'cellar', 'north': 'bedRoom', 'south': 'exit' },
     'description': 'The great hall at the villages center. There is a weapon cache at the back', 'items': ['crossbow', 'shield']
   },
-  'bedRoom': {name: 'BedRoom', 'directions': { 'south': ['greatHall'] }, 'description': "The chiefs room, although he is no where to be found", 'items': ['armory', 'key'] },
-  'cellar': {name: 'Cellar', 'directions': { 'west': ['greatHall'] }, 'description': "It smells foul and damp in here. The cries of prisoners can be heard echoing on the cracked stone walls.", 'items': ['chestplate'] },
-  'exit': {name: 'Exit', 'directions': { 'north': ['greatHall'] }, 'description': "Goodbye! You can type leave to end the game.", 'items': [] },
+  'bedRoom': { name: 'BedRoom', 'directions': { 'south': ['greatHall'] }, 'description': "The chiefs room, although he is no where to be found", 'items': ['armory', 'key'] },
+  'cellar': { name: 'Cellar', 'directions': { 'west': ['greatHall'] }, 'description': "It smells foul and damp in here. The cries of prisoners can be heard echoing on the cracked stone walls.", 'items': ['chestplate'] },
+  'exit': { name: 'Exit', 'directions': { 'north': ['greatHall'] }, 'description': "Goodbye! You can type leave to end the game.", 'items': [] },
 }
+exports.rooms = { rooms }
 
 storyIntro()
 
-// const greatHallDescription = `You are located at: ${rooms.greatHall.name} -=- ${rooms['greatHall'].description} 
-// Objects:
-// ${rooms['greatHall'].items}`//Crossbow, Shield
-// log(chalk.rgb(145, 141, 141)(greatHallDescription))
+let currentRoom = rooms.greatHall;
 
-// let nextMove = prompt("What's your next move? ");//walk west
-// log(chalk.rgb(145, 141, 141)("-=- walk", (nextMove)));
+let currentLocation = generateDescription(currentRoom.name, currentRoom.description, currentRoom.items);
 
-// const bedRoomDescription = `You are located at: ${rooms.bedRoom.name} -=- ${rooms['bedRoom'].description} 
-// Objects:
-// ${rooms['bedRoom'].items}`
+let nextMove = prompt("What's your next move? ");//walk west
+log(chalk.rgb(145, 141, 141)("-=- walk", nextMove));
 
-// const cellarDescription = `You are located at: ${rooms.cellar.name} -=- ${rooms['cellar'].description} 
-// Objects:
-// ${rooms['cellar'].items}`
+function notAvailableDirection() {
+  let availableDirections = Object.keys(currentRoom.directions);
+  // console.log(availableDirections)
+  const isDirectionAvailable = availableDirections.includes(nextMove)
+  if (!isDirectionAvailable) {
+    console.log("Try again, your currently available exits are", currentRoom.directions)
+    nextMove = prompt("What's your next move? ");//walk west
+  }
+}
+notAvailableDirection()
 
-// const exitDescription = `You are located at: ${rooms.exit.name} -=- ${rooms['exit'].description}`
 
-// function directionMove() {
-//   if (nextMove == "walk east") {
-//     console.log(cellarDescription)
-//   }
-//   else if (nextMove == "walk north") {
-//     console.log(bedRoomDescription)
-//   }
-//   else if (nextMove == "walk south") {
-//     console.log(exitDescription)
-//   }
-//   else {
-//     console.log("Try again, your currently available exits are", (rooms['greatHall'].directions))
-//   }
-// }
-// directionMove()
-
-// nextMove = prompt("What's your next move? ");
-// log(chalk.rgb(145, 141, 141)("-=- walk", (nextMove)));
-
-// log(chalk.green(bedRoomDescription))
-
-// console.log("-=- collect armory")
-// console.log("armory has been added to your inventory")
-
-// console.log("-=- collect key")
-// console.log("key has been added to your inventory")
-
-// nextMove = prompt("What's your next move? ");
-// log(chalk.rgb(145, 141, 141)("-=- walk", (nextMove)));
-
-// log(chalk.green(cellarDescription))
-
-// console.log("-=- collect chestplate")
-// console.log("chestplate has been added to your inventory")
-
-// nextMove = prompt("What's your next move? ");
-// log(chalk.rgb(145, 141, 141)("-=- walk", (nextMove)));
-
-// log(chalk.green(exitDescription))
 
 
 
