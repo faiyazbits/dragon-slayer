3ce3f63ad42326d362201c90d7943e48a5c60dad
jroshansafi <jroshansafi@gmail.com>
2022-04-26 14:53:02 +0530

    added items in the inventory

--- description.js
+++ description.js
@@ -2,13 +2,14 @@ const chalk = require('chalk');
 const log = console.log;
 
 function generateDescription(rooms){
-console.log(`You are located at: ${rooms.name} -=- ${rooms.description} 
+console.log(chalk.blue(`You are located at: ${rooms.name} -=- ${rooms.description} 
 Objects:
-${rooms.items}`)
+${rooms.items}`))//chestplate, shield
+
 }
 
-function isAvailableDirection(rooms){
+function logAvailableDirection(rooms){
 console.log("Try again, your currently available exits are", rooms.directions)
 }
 
-module.exports = {generateDescription, isAvailableDirection};
+module.exports = {generateDescription, logAvailableDirection};
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const delayedLog = require('./delayedLog')
 const rooms = require('./rooms')
 const storyIntro = require('./intoduction');
-const { generateDescription, isAvailableDirection } = require('./description');
+const { generateDescription, logAvailableDirection } = require('./description');
+const inventoryDescription = require('./inventory');
+
 
 const prompt = require('prompt-sync')();
 const chalk = require('chalk');
@@ -10,21 +12,27 @@ const log = console.log;
 async function main() {
   await storyIntro();
   let currentRoom = rooms.greatHall;
-  let gameOver = false;
   generateDescription(currentRoom)
+  let gameOver = false;
 
   while (!gameOver) {
+
+    inventoryDescription(currentRoom)
+
     const userInput = prompt("What's your next move? ");
     log(chalk.rgb(145, 141, 141)("-=-", userInput));
     const direction = currentLocation(userInput)
 
     currentRoom = navigateDirection(currentRoom, direction)
-    if(currentRoom.name == "Exit"){
-      gameOver = true;
-    }
 
-    generateDescription(currentRoom)
-    console.log("you are in exit, Game End")
+    if (currentRoom.name !== 'Exit') {
+      generateDescription(currentRoom)
+    }
+    else {
+      gameOver = true;
+      generateDescription(currentRoom)
+      console.log("you are in exit, Game End")
+    }
 
   }
 }
@@ -34,18 +42,17 @@ function currentLocation(userInput) {
   return currentOut[1]
 }
 
-function navigateDirection(currentRoom,direction){
+function navigateDirection(currentRoom, direction) {
 
-  if(currentRoom.directions[direction]){
+  if (currentRoom.directions[direction]) {
     const roomName = currentRoom.directions[direction]
-    return rooms[roomName]  
-   }
-  else{
-    isAvailableDirection(currentRoom)
+    return rooms[roomName]
+  }
+  else {
+    logAvailableDirection(currentRoom)
     return currentRoom
   }
 }
-
 
 main()
 
